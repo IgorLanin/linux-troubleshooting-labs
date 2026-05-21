cdb8d94ae30a560bb1528a3a8b0e7194da7c3f33
IgorLanin <lanin.igor@gmail.com>
2026-05-21 11:44:27 +0800

    Инцидент №2: добавил скрипты

---
+++ scripts/lab_2/incident_2.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -e
+
+############################################################
+# Print colored messages
+# Usage: green, red, default color
+############################################################
+function print_color() {
+    no_color="\e[0;37m"
+
+    case $1 in
+        green)
+            color="\e[0;32m"
+        ;;
+        red)
+            color="\e[0;31m"
+        ;;
+        *)
+            color=no_color
+        ;;
+    esac
+
+    echo -e "${color} $2 ${no_color}"
+}
+
+
+
+############################################################
+# Check if service is enabled active
+# Usage: nginx
+############################################################
+function check_service_status() {
+    is_service_active=$(sudo systemctl is-active "$1")
+
+    if [[ $is_service_active == "active" ]]
+    then
+        print_color "green" "Service ${1} is active"
+    else
+        print_color "red" "Service ${1} is not active"
+        exit 1
+    fi
+}
+
+
+############################################################
+# Main setup
+############################################################
+
+print_color "green" "----------Break backend...----------"
+pkill -f app.py
+curl localhost
---
+++ scripts/lab_2/setup.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+set -e
+
+############################################################
+# Print colored messages
+# Usage: green, red, default color
+############################################################
+function print_color() {
+    no_color="\e[0;37m"
+
+    case $1 in
+        green)
+            color="\e[0;32m"
+        ;;
+        red)
+            color="\e[0;31m"
+        ;;
+        *)
+            color=no_color
+        ;;
+    esac
+
+    echo -e "${color} $2 ${no_color}"
+}
+
+
+
+############################################################
+# Check if service is enabled active
+# Usage: nginx
+############################################################
+function check_service_status() {
+    is_service_active=$(sudo systemctl is-active "$1")
+
+    if [[ $is_service_active == "active" ]]
+    then
+        print_color "green" "Service ${1} is active"
+    else
+        print_color "red" "Service ${1} is not active"
+        exit 1
+    fi
+}
+
+
+############################################################
+# Main setup
+############################################################
+
+print_color "green" "----------Start configuring...----------"
+sudo apt update -y
+
+
+print_color "green" "----------Install and enable nginx...----------"
+sudo apt install -y nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+check_service_status nginx
+
+print_color "green" "----------Installing python3-flack...----------"
+sudo apt install python3-flask -y
+python --version
+
+
+print_color "green" "----------Run backend...----------"
+python3 ~/linux-troubleshooting-labs/scripts/lab_2/app.py
+
+
+print_color "green" "----------Create nginx reverse proxy...----------"
+sudo cp ~/linux-troubleshooting-labs/scripts/lab_2/flackapp.conf /etc/nginx/sites-available/flackapp.conf
+
+sudo ln -s /etc/nginx/sites-available/flackapp.conf /etc/nginx/sites-enabled/flackapp.conf
+sudo nginx -t
+sudo systemctl restart nginx
+
+
+print_color "green" "----------Check backend availability...----------"
+curl localhost
