c56f113a50eb02ea8e51b885e4bf5187a9e1d3ff
IgorLanin <lanin.igor@gmail.com>
2026-05-26 17:33:48 +0800

    Инцидент №4: внес исправления в описание, скрипт и общий README

--- scripts/lab_4/incident_4.sh
+++ scripts/lab_4/incident_4.sh
@@ -27,33 +27,19 @@ function print_color() {
 
 
 ############################################################
-# Check if service is enabled active
-# Usage: nginx
-############################################################
-function check_service_status() {
-    is_service_active=$(sudo systemctl is-active "$1")
-
-    if [[ $is_service_active == "active" ]]
-    then
-        print_color "green" "Service ${1} is active"
-    else
-        print_color "red" "Service ${1} is not active"
-        exit 1
-    fi
-}
-
-
-############################################################
 # Main setup
 ############################################################
 
-read -p "Make up a user name for the new user: >" $username
+read -p "Make up a user name for the new user: > " username
 
 print_color "green" "----------Create the new user "$username"...----------"
 sudo useradd -m "$username"
 
 print_color "green" "----------Make up a password for the new user "$username"...----------"
 sudo passwd "$username"
+sudo usermod -aG sudo "$username"
 
-print_color "green" "----------Incident_reproduction...----------"
-sudo usermod -s /bin/fake_shell "$username"
+print_color "green" "----------Incident reproduction...----------"
+sudo usermod -s /bin/false "$username"
+
+print_color "green" "----------Incident is reproduced for user "$username"...----------"
